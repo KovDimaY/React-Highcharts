0f09c5d4c79963975b792f3ab6d542daf6e0f388
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2017-08-20 16:31:03 +0200

    Initial state moved to the constants

--- src/constants/line/modes-options.js
+++ src/constants/line/modes-options.js
@@ -5,6 +5,51 @@ export const modes = {
   interestingFacts: "Interesting Facts"
 }
 
+export const initialState = {
+  options: {},
+  rerenderChart: false,
+  currentMode: modes.pureRandom,
+  configurations: {
+    pureRandom: {
+      tooltip: true,
+      zoom: true,
+      legend: true,
+      title: true,
+      dataLabels: true,
+      animation: true,
+      yAxisTitle: true,
+      markers: true,
+    },
+    polinomials: {
+      min: -10,
+      max: 10,
+      number: 100,
+      linearA: 1,
+      linearB: 1,
+      quadraticA: 1,
+      quadraticB: 1,
+      quadraticC: 1,
+      cubicA: 1,
+      cubicB: 1,
+      cubicC: 1,
+      cubicD: 1
+    },
+    trigonometric: {
+      min: -10,
+      max: 10,
+      number: 100,
+      cosA: 1,
+      cosB: 1,
+      sinA: 1,
+      sinB: 1,
+      tanA: 1,
+      tanB: 1,
+      ctanA: 1,
+      ctanB: 1
+    }
+  }
+}
+
 export const optionsPureRandom = {
   tooltip: "tooltip",
   zoom: "zoom",
