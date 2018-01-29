418fabb82de4cadb56f05853663124ba7845cff3
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-01-29 22:31:17 +0100

    DEV-19 fixed tabulation

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,9 +12,9 @@ export default class Footer extends Component {
 
   						<div className="social-media-icons">
 								<a target="_blank" href="https://github.com/KovDimaY/React-Highcharts" className="footer-github"><i className="fa fa-github"></i></a>
-  							<a target="_blank" href="https://www.facebook.com/dmytro.kovalenko.1004" className="footer-facebook"><i className="fa fa-facebook"></i></a>
-  							<a target="_blank" href="https://www.vk.com/id11545172" className="footer-vk"><i className="fa fa-vk"></i></a>
-  							<a target="_blank" href="https://www.linkedin.com/in/kovalenkodmytro" className="footer-linkedin"><i className="fa fa-linkedin"></i></a>
+								<a target="_blank" href="https://www.facebook.com/dmytro.kovalenko.1004" className="footer-facebook"><i className="fa fa-facebook"></i></a>
+								<a target="_blank" href="https://www.vk.com/id11545172" className="footer-vk"><i className="fa fa-vk"></i></a>
+								<a target="_blank" href="https://www.linkedin.com/in/kovalenkodmytro" className="footer-linkedin"><i className="fa fa-linkedin"></i></a>
   						</div>
   					</div>
   				</div>
