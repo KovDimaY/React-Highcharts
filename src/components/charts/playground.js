317a601b15ed018178094bfd8a1553d0d5789a4e
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2018-04-19 12:12:59 +0200

    DEV-31 finished the playground section

--- src/components/charts/playground.js
+++ src/components/charts/playground.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Highcharts from 'highcharts'
-import JSONEditor from 'jsoneditor'
 
 import Chart from './chart-abstract'
-
-const defaultOptions = {
-	chart: {
-		height: '400px',
-	},
-	title: {
-			text: 'Solar Employment Growth by Sector, 2010-2016'
-	},
-	legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle'
-	},
-	series: [{
-			name: 'Installation',
-			data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-	}, {
-			name: 'Manufacturing',
-			data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-	}, {
-			name: 'Sales & Distribution',
-			data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-	}, {
-			name: 'Project Development',
-			data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-	}, {
-			name: 'Other',
-			data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-	}],
-};
 
 export default class Playground extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			options: defaultOptions
+			options: this.generateInitialOptions(),
+		};
+		this.editor = undefined;
+
+		this.updateChartBasedOnJSON = this.updateChartBasedOnJSON.bind(this);
+	}
+
+	generateInitialOptions() {
+		const generateRandomData = (count) => {
+			const result = [];
+			for (let i = 0; i < count; i += 1) {
+				const randValue = Math.random() * 1000;
+				result.push(Math.round(randValue * 100) / 100);
+			}
+			return result;
+		};
+		
+		return {
+			title: {
+				text: 'Simple example'
+			},
+			series: [{
+				name: 'Serie 1',
+				data: generateRandomData(10),
+			}, {
+				name: 'Serie 2',
+				data: generateRandomData(10),
+			}, {
+				name: 'Serie 3',
+				data: generateRandomData(10),
+			}],
 		};
 	}
 
@@ -48,28 +47,32 @@ export default class Playground extends Component {
 		var options = {
 			mode: 'code'
 		};
-		const editor = new JSONEditor(container, options);
-		editor.set({
-			"Array": [1, 2, 3],
-			"Boolean": true,
-			"Null": null,
-			"Number": 123,
-			"Object": {"a": "b", "c": "d"},
-			"String": "Hello World"
-		});
+		this.editor = new JSONEditor(container, options);
+		this.editor.set(this.state.options);
+	}
+
+	updateChartBasedOnJSON() {
+		try {
+			const newOptions = this.editor.get();
+			this.setState({ options: newOptions, rerenderChart: true }, () => {
+				this.setState({ rerenderChart: false })
+			});
+		} catch (e) {
+			alert("Your JSON has some errors. Please check your input and try again.");
+		}
+		
 	}
 
 	renderConfigurationsArea() {
 		return (
 		  <div className="other-clock-container">
 			<div className="checkboxes other-clock">
-			  CLOCK CONFIG
-				<div id="jsoneditor" style={{width: "400px", height: "400px"}}></div>
+				<div id="jsoneditor"></div>
 			</div>
 			<button
 			  type="button"
 			  className="btn btn-success apply-button position-dynamic"
-			  onClick={() => {}}>
+			  onClick={this.updateChartBasedOnJSON}>
 			  Apply
 			</button>
 		  </div>
