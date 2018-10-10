1c77f2b5d74722c3a43d2dddede97866e689b134
Dmytro Kovalenko <missklimchuck@yandex.ua>
2018-10-10 23:18:50 +0200

    DEV-20 fixed the problem of Pie chart and cleaned dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 // Node modules import
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
-import reduxThunk from 'redux-thunk';
 
 // Components import
 import routes from './routes';
@@ -13,7 +10,6 @@ import routes from './routes';
 import '../styles/index.scss';
 
 ReactDOM.render(
-  <Provider>
-    <Router history={browserHistory} routes={routes} />
-  </Provider>, document.querySelector('#react-app')
+  <Router history={browserHistory} routes={routes} />,
+  document.querySelector('#react-app')
 );
