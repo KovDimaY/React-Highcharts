bd8cd9a4c015e612288168ffe2bd790a2f2126a6
Dmytro Kovalenko <kovalenko.d.y@gmail.com>
2020-10-14 22:27:54 +0200

    DEV-36 applied prettier to the root files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port, () => console.log(`Server is running on the localhost:${port}`.rainbow))
+app.listen(port, () => console.log(`Server is running on the localhost:${port}`.rainbow));
